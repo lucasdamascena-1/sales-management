60c55f314902d4077b29569a6838c6cb6627ae27
Lucas Gabriel <lucas.gdsouza@outlook.com>
2020-04-22 20:52:47 -0300

    Login Layout

--- SalesManagement/Controllers/HomeController.cs
+++ SalesManagement/Controllers/HomeController.cs
@@ -6,6 +6,11 @@ namespace SalesManagement.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Index()
         {
             return View();
